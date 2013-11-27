be6b22c2faee8dcd2f433cfcf3ef46502c3c85c0
Aman Gupta <aman@tmm1.net>
2013-11-27 01:58:59 -0800

    enable check mode

--- gc.c
+++ gc.c
@@ -181,7 +181,7 @@ static ruby_gc_params_t gc_params = {
  * 3: show all references
  */
 #ifndef RGENGC_CHECK_MODE
-#define RGENGC_CHECK_MODE  0
+#define RGENGC_CHECK_MODE  3
 #endif
 
 /* RGENGC_PROFILE
