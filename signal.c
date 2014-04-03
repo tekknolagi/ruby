2a4e085569d2dcb8177f35ba00e59b8a42eb7c90
Scott Francis <scott.francis@shopify.com>
2014-04-03 13:51:12 +0000

    Dump x64 machine registers on sigsegv or sigbus
    Conflicts:
    	vm_dump.c
    
    Conflicts:
    	vm_dump.c

--- signal.c
+++ signal.c
@@ -652,6 +652,8 @@ check_stack_overflow(const void *addr)
 #define MESSAGE_FAULT_ADDRESS
 #endif
 
+void * ruby_signal_ctx = NULL;
+
 #ifdef SIGBUS
 static RETSIGTYPE
 sigbus(int sig SIGINFO_ARG)
@@ -664,6 +666,7 @@ sigbus(int sig SIGINFO_ARG)
 #if defined __APPLE__
     CHECK_STACK_OVERFLOW();
 #endif
+    ruby_signal_ctx = ctx;
     rb_bug("Bus Error" MESSAGE_FAULT_ADDRESS);
 }
 #endif
@@ -701,6 +704,7 @@ sigsegv(int sig SIGINFO_ARG)
 
     segv_received = 1;
     ruby_disable_gc_stress = 1;
+    ruby_signal_ctx = ctx;
     rb_bug("Segmentation fault" MESSAGE_FAULT_ADDRESS);
 }
 #endif
