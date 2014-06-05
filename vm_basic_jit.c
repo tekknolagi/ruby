f67ac98bcb2be5c7d77df0a316381bc5afec0392
Francis Bogsanyi <francis.bogsanyi@shopify.com>
2014-06-05 17:56:17 +0000

    Initial cut at alloc + compile

--- vm_basic_jit.c
+++ vm_basic_jit.c
@@ -1,6 +1,16 @@
 #if OPT_BASIC_JIT
+struct rb_jit_free_list {
+    size_t size;
+    struct rb_jit_free_list *next;
+};
+
 struct rb_jit_code_cache {
-    void *start;
+    size_t size;
+    struct rb_jit_code_cache *next;
+    struct rb_jit_free_list *free_list;
+};
+
+struct rb_jit_code_chunk {
     size_t size;
 };
 
@@ -29,19 +39,43 @@ rb_iseq_free_jit_compiled_iseq(void *jit_compiled_iseq)
     */
 }
 
-static void *
-rb_iseq_allocate_jit_compiled_iseq(unsigned long size)
+static struct rb_jit_code_cache *
+rb_iseq_allocate_jit_compiled_iseq(rb_iseq_t *iseq, size_t size)
 {
-    /*
-    TODO: allocate chunk of size from jit code cache
-    */
+    rb_vm_t *vm = GET_VM();
+    struct rb_jit_code_cache *cache = vm->jit_code_cache;
+    if (size == 0) {
+        return 0;
+    }
+    while (cache) {
+        struct rb_jit_free_list **free_list_ptr = &cache->free_list;
+        struct rb_jit_free_list *free_list = *free_list_ptr;
+        while (free_list) {
+            if (free_list->size >= size) {
+                struct rb_jit_code_chunk *chunk = (struct rb_jit_code_chunk *) free_list;
+                if (free_list->size > size + sizeof(rb_jit_code_chunk)) {
+                    /* TODO add remainder to free list */
+                } else {
+                    *free_list_ptr = free_list->next;
+                }
+                chunk->size = size + sizeof(struct rb_jit_code_chunk *);
+                iseq->jit_compiled_iseq = (void *)(chunk + 1);
+                return cache;
+            }
+            free_list_ptr = &free_list->next
+            free_list = *free_list_ptr;
+        }
+        cache = cache->next;
+    }
+    /* TODO allocate a new JIT code cache region */
+    return cache;
 }
 
-static unsigned long
+static size_t
 rb_iseq_jit_compiled_size(rb_iseq_t *iseq, const void * const *insns_address_table, void **end_insns)
 {
-    unsigned long size = 0;
-    unsigned long i = 0;
+    size_t size = 0;
+    size_t i = 0;
     while (i < iseq->iseq_size) {
         int insn = (int)iseq->iseq[i];
         char *beg = (char *)insns_address_table[insn];
@@ -59,10 +93,31 @@ rb_iseq_jit_compiled_size(rb_iseq_t *iseq, const void * const *insns_address_tab
 static int
 rb_iseq_jit_compile(rb_iseq_t *iseq, const void * const *insns_address_table, void **end_insns)
 {
-    /*
-    TODO: compute the compiled iseq size, allocate chunk from jit code cache, copy into chunk
-    copy by walking instructions in iseq, copying from insns_address_table[i] to insns_address_table[i+1]
-    */
+    size_t size = rb_iseq_jit_compiled_size(iseq, insns_address_table, end_insns);
+    struct rb_jit_code_cache *cache = rb_iseq_allocate_jit_compiled_iseq(iseq, size);
+    void *code = iseq->jit_compiled_iseq;
+    size_t i = 0;
+
+    if (code == 0) {
+        return -1;
+    }
+
+    enable_write_in_jit_code_cache(cache);
+    while (i < iseq->iseq_size) {
+        int insn = (int)iseq->iseq[i];
+        char *beg = (char *)insns_address_table[insn];
+        char *end = (char *)end_insns;
+        if (insn + 1 < VM_INSTRUCTION_SIZE) {
+            end = (char *)insns_address_table[insn + 1];
+        }
+        /* TODO something special for branch instructions? */
+        memcpy(code, beg, end - beg);
+        code = (void *)((char *)code + (end - beg));
+        i += insn_len(insn);
+    }
+    enable_execution_in_jit_code_cache(cache);
+
+    return 0;
 }
 
 static int
@@ -98,6 +153,7 @@ vm_exec_jit(rb_thread_t *th, VALUE initial)
 
   first:
     goto *reg_cfp->iseq->jit_compiled_iseq;
+    /* TODO define macros (see vm_exec.h) for vm.inc */
 /*****************/
  #include "vm.inc"
 /*****************/
