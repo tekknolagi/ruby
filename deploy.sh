d15f8f0da5ab3480f839a52e164056eac09a5c27
Adam Richardson <adam.richardson@shopify.com>
2016-03-24 11:26:44 -0400

    Change upload script name

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ SHA=`cat build.version` make -f Makefile.pbuilder
 ls artifacts/*
 for DISTRO in $(ls artifacts)
 do
-  upload_to_packagecloud.sh $DISTRO $(ls artifacts/$DISTRO/*.deb)
+  push_debs_to_stable.sh $DISTRO $(ls artifacts/$DISTRO/*.deb)
 done
