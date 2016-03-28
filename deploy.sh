259d2daaf4b6acdc6d6aafd15fdff501722518f8
Adam Richardson <adam.richardson@shopify.com>
2016-03-28 17:23:25 -0400

    New deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xeuo pipefail
-
-SHA=`cat build.version` make -f Makefile.pbuilder
-ls artifacts/*
-for DISTRO in $(ls artifacts)
-do
-  push_debs_to_stable.sh $DISTRO $(ls artifacts/$DISTRO/*.deb)
-done
