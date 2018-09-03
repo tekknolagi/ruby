bb433e44b3202306c3713cba818a279fc704c7b5
Darren Worrall <darren.worrall@shopify.com>
2018-09-03 11:25:43 +0100

    WIP

--- bin/ci/build.sh
+++ bin/ci/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-NAME=${BUILDKITE_TAG:-$(cat NAME_TO_BUILD 2>/dev/null)}
+NAME=$(cat NAME_TO_BUILD 2>/dev/null)
 VERSION=${VERSION:-$(cat VERSION 2>/dev/null || echo 1)}
 
-if [[ -z "${NAME}" || "${NAME}" == "ruby-shopify-" ]]; then
+if [[ -z "${NAME}" || "${NAME}" == "ruby-shopify-X.Y.Z" ]]; then
   >&2 echo "Build name must be set, check the README"
   exit 1
 fi
