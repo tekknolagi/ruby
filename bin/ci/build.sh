4dbe5a8e9d0aefe3ed97d70d2622b4a919e4b228
Darren Worrall <darren.worrall@shopify.com>
2018-10-25 13:16:27 +0100

    CI hack

--- bin/ci/build.sh
+++ bin/ci/build.sh
@@ -2,10 +2,10 @@
 NAME=$(cat NAME_TO_BUILD 2>/dev/null)
 VERSION=${VERSION:-$(cat VERSION 2>/dev/null || echo 1)}
 
-if [[ -z "${NAME}" || "${NAME}" == "ruby-shopify-X.Y.Z" ]]; then
-  >&2 echo "Build name must be set, check the README"
-  exit 1
-fi
+#if [[ -z "${NAME}" || "${NAME}" == "ruby-shopify-X.Y.Z" ]]; then
+#  >&2 echo "Build name must be set, check the README"
+#  exit 1
+#fi
 
 git clone git@github.com:shopify/ruby.git #TODO(DazWorrall): In lieu of history: full
-cd ruby && ../ruby_build --name "${NAME}" --version "${VERSION}"
+cd ruby && ../ruby_build --name "ruby-shopify-2.5.3" --version "${VERSION}"
