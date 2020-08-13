c52d5eadfba95fe1060b4302c2be18c654569168
Jean Boussier <jean.boussier@gmail.com>
2020-08-13 19:36:22 +0200

    ObjectSpace.dump_all buffer writes for improved performance

--- ext/objspace/objspace_dump.c
+++ ext/objspace/objspace_dump.c
@@ -27,6 +27,9 @@ static VALUE sym_full;
 
 struct dump_config {
     VALUE type;
+    char *buffer;
+    unsigned long buffer_len;
+    unsigned long buffer_capa;
     FILE *stream;
     VALUE string;
     const char *root_category;
@@ -37,21 +40,75 @@ struct dump_config {
     unsigned int full_heap: 1;
 };
 
-PRINTF_ARGS(static void dump_append(struct dump_config *, const char *, ...), 2, 3);
 static void
-dump_append(struct dump_config *dc, const char *format, ...)
+dump_flush(struct dump_config *dc)
 {
-    va_list vl;
-    va_start(vl, format);
-
-    if (dc->stream) {
-	vfprintf(dc->stream, format, vl);
+    if (dc->buffer_len) {
+        if (dc->stream) {
+            fwrite(dc->buffer, 1, dc->buffer_len, dc->stream);
+        } else if (dc->string) {
+            rb_str_cat(dc->string, dc->buffer, dc->buffer_len);
+        }
+        dc->buffer_len = 0;
     }
-    else if (dc->string)
-	rb_str_vcatf(dc->string, format, vl);
+}
+
+static void buffer_inc_capa(struct dump_config *dc, unsigned long requested)
+{
+    unsigned long required;
+
+    if (requested + dc->buffer_len > dc->buffer_capa) {
+        dump_flush(dc);
+    }
+
+    for (required = dc->buffer_capa; requested > required - dc->buffer_len; required <<= 1);
+
+    if (required > dc->buffer_capa) {
+        REALLOC_N(dc->buffer, char, required);
+        dc->buffer_capa = required;
+    }
+}
+
+static void buffer_append(struct dump_config *dc, const char *cstr, unsigned long len)
+{
+    if (len > 0) {
+        buffer_inc_capa(dc, len);
+        MEMCPY(dc->buffer + dc->buffer_len, cstr, char, len);
+        dc->buffer_len += len;
+    }
+}
+
+# define dump_append(dc, str) buffer_append(dc, (str), (long)strlen(str))
+
+PRINTF_ARGS(static void dump_appendf(struct dump_config *, const char *, ...), 2, 3);
+static void
+dump_appendf(struct dump_config *dc, const char *format, ...)
+{
+    va_list vl, vl2;
+    va_start(vl, format);
+    va_copy(vl2, vl);
+
+    unsigned long capa_left = dc->buffer_capa - dc->buffer_len;
+    unsigned long required = vsnprintf(dc->buffer + dc->buffer_len, capa_left, format, vl);
+
+    if (required >= capa_left) {
+        buffer_inc_capa(dc, required + 1);
+        capa_left = dc->buffer_capa - dc->buffer_len;
+        vsnprintf(dc->buffer + dc->buffer_len, capa_left, format, vl2);
+    }
+    dc->buffer_len += required;
 
     va_end(vl);
 }
+
+static void
+dump_appendc(struct dump_config *dc, char c)
+{
+    buffer_inc_capa(dc, 1);
+    dc->buffer[dc->buffer_len] = c;
+    dc->buffer_len++;
+}
+
 
 static void
 dump_append_string_value(struct dump_config *dc, VALUE obj)
@@ -64,8 +121,9 @@ dump_append_string_value(struct dump_config *dc, VALUE obj)
     for (i = 0, value = RSTRING_PTR(obj); i < RSTRING_LEN(obj); i++) {
 	switch ((c = value[i])) {
 	  case '\\':
+	    dump_append(dc, "\\\\");
 	  case '"':
-	    dump_append(dc, "\\%c", c);
+	    dump_append(dc, "\\\"");
 	    break;
 	  case '\0':
 	    dump_append(dc, "\\u0000");
@@ -87,9 +145,9 @@ dump_append_string_value(struct dump_config *dc, VALUE obj)
 	    break;
 	  default:
 	    if (c <= 0x1f)
-		dump_append(dc, "\\u%04d", c);
+		dump_appendf(dc, "\\u%04d", c);
 	    else
-		dump_append(dc, "%c", c);
+		dump_appendc(dc, c);
 	}
     }
     dump_append(dc, "\"");
@@ -153,10 +211,10 @@ dump_append_special_const(struct dump_config *dc, VALUE value)
 	dump_append(dc, "null");
     }
     else if (FIXNUM_P(value)) {
-	dump_append(dc, "%ld", FIX2LONG(value));
+	dump_appendf(dc, "%ld", FIX2LONG(value));
     }
     else if (FLONUM_P(value)) {
-	dump_append(dc, "%#g", RFLOAT_VALUE(value));
+	dump_appendf(dc, "%#g", RFLOAT_VALUE(value));
     }
     else if (SYMBOL_P(value)) {
 	dump_append_symbol_value(dc, value);
@@ -164,6 +222,7 @@ dump_append_special_const(struct dump_config *dc, VALUE value)
     else {
 	dump_append(dc, "{}");
     }
+    dump_flush(dc);
 }
 
 static void
@@ -175,9 +234,9 @@ reachable_object_i(VALUE ref, void *data)
 	return;
 
     if (dc->cur_obj_references == 0)
-        dump_append(dc, ", \"references\":[\"%#"PRIxVALUE"\"", ref);
+        dump_appendf(dc, ", \"references\":[\"%#"PRIxVALUE"\"", ref);
     else
-        dump_append(dc, ", \"%#"PRIxVALUE"\"", ref);
+        dump_appendf(dc, ", \"%#"PRIxVALUE"\"", ref);
 
     dc->cur_obj_references++;
 }
@@ -185,9 +244,9 @@ reachable_object_i(VALUE ref, void *data)
 static void
 dump_append_string_content(struct dump_config *dc, VALUE obj)
 {
-    dump_append(dc, ", \"bytesize\":%ld", RSTRING_LEN(obj));
+    dump_appendf(dc, ", \"bytesize\":%ld", RSTRING_LEN(obj));
     if (!STR_EMBED_P(obj) && !STR_SHARED_P(obj) && (long)rb_str_capacity(obj) != RSTRING_LEN(obj))
-	dump_append(dc, ", \"capacity\":%"PRIuSIZE, rb_str_capacity(obj));
+	dump_appendf(dc, ", \"capacity\":%"PRIuSIZE, rb_str_capacity(obj));
 
     if (is_ascii_string(obj)) {
 	dump_append(dc, ", \"value\":");
@@ -216,87 +275,89 @@ dump_object(VALUE obj, struct dump_config *dc)
     if (dc->cur_obj == dc->string)
 	return;
 
-    dump_append(dc, "{\"address\":\"%#"PRIxVALUE"\", \"type\":\"%s\"", obj, obj_type(obj));
+    dump_appendf(dc, "{\"address\":\"%#"PRIxVALUE"\", \"type\":\"%s\"", obj, obj_type(obj));
 
     if (dc->cur_obj_klass)
-        dump_append(dc, ", \"class\":\"%#"PRIxVALUE"\"", dc->cur_obj_klass);
+        dump_appendf(dc, ", \"class\":\"%#"PRIxVALUE"\"", dc->cur_obj_klass);
     if (rb_obj_frozen_p(obj))
-	dump_append(dc, ", \"frozen\":true");
+        dump_append(dc, ", \"frozen\":true");
 
     switch (BUILTIN_TYPE(obj)) {
       case T_NONE:
-	dump_append(dc, "}\n");
-	return;
+        dump_append(dc, "}\n");
+        dump_flush(dc);
+        return;
 
       case T_IMEMO:
-	dump_append(dc, ", \"imemo_type\":\"%s\"", rb_imemo_name(imemo_type(obj)));
-	break;
+        dump_appendf(dc, ", \"imemo_type\":\"%s\"", rb_imemo_name(imemo_type(obj)));
+        break;
 
       case T_SYMBOL:
-	dump_append_string_content(dc, rb_sym2str(obj));
-	break;
+        dump_append_string_content(dc, rb_sym2str(obj));
+        break;
 
       case T_STRING:
-	if (STR_EMBED_P(obj))
-	    dump_append(dc, ", \"embedded\":true");
-	if (is_broken_string(obj))
-	    dump_append(dc, ", \"broken\":true");
-	if (FL_TEST(obj, RSTRING_FSTR))
-	    dump_append(dc, ", \"fstring\":true");
-	if (STR_SHARED_P(obj))
-	    dump_append(dc, ", \"shared\":true");
-	else
-	    dump_append_string_content(dc, obj);
+        if (STR_EMBED_P(obj))
+            dump_append(dc, ", \"embedded\":true");
+        if (is_broken_string(obj))
+            dump_append(dc, ", \"broken\":true");
+        if (FL_TEST(obj, RSTRING_FSTR))
+            dump_append(dc, ", \"fstring\":true");
+        if (STR_SHARED_P(obj))
+            dump_append(dc, ", \"shared\":true");
+        else
+            dump_append_string_content(dc, obj);
 
-	if (!ENCODING_IS_ASCII8BIT(obj))
-	    dump_append(dc, ", \"encoding\":\"%s\"", rb_enc_name(rb_enc_from_index(ENCODING_GET(obj))));
-	break;
+        if (!ENCODING_IS_ASCII8BIT(obj))
+            dump_appendf(dc, ", \"encoding\":\"%s\"", rb_enc_name(rb_enc_from_index(ENCODING_GET(obj))));
+        break;
 
       case T_HASH:
-	dump_append(dc, ", \"size\":%"PRIuSIZE, (size_t)RHASH_SIZE(obj));
+        dump_appendf(dc, ", \"size\":%"PRIuSIZE, (size_t)RHASH_SIZE(obj));
         if (FL_TEST(obj, RHASH_PROC_DEFAULT))
-            dump_append(dc, ", \"default\":\"%#"PRIxVALUE"\"", RHASH_IFNONE(obj));
-	break;
+            dump_appendf(dc, ", \"default\":\"%#"PRIxVALUE"\"", RHASH_IFNONE(obj));
+        break;
 
       case T_ARRAY:
-	dump_append(dc, ", \"length\":%ld", RARRAY_LEN(obj));
-	if (RARRAY_LEN(obj) > 0 && FL_TEST(obj, ELTS_SHARED))
-	    dump_append(dc, ", \"shared\":true");
-	if (RARRAY_LEN(obj) > 0 && FL_TEST(obj, RARRAY_EMBED_FLAG))
-	    dump_append(dc, ", \"embedded\":true");
-	break;
+        dump_appendf(dc, ", \"length\":%ld", RARRAY_LEN(obj));
+        if (RARRAY_LEN(obj) > 0 && FL_TEST(obj, ELTS_SHARED))
+            dump_append(dc, ", \"shared\":true");
+        if (RARRAY_LEN(obj) > 0 && FL_TEST(obj, RARRAY_EMBED_FLAG))
+            dump_append(dc, ", \"embedded\":true");
+        break;
 
       case T_CLASS:
       case T_MODULE:
-	if (dc->cur_obj_klass) {
-	    VALUE mod_name = rb_mod_name(obj);
-	    if (!NIL_P(mod_name))
-		dump_append(dc, ", \"name\":\"%s\"", RSTRING_PTR(mod_name));
-	}
-	break;
+        if (dc->cur_obj_klass) {
+            VALUE mod_name = rb_mod_name(obj);
+            if (!NIL_P(mod_name))
+        	dump_appendf(dc, ", \"name\":\"%s\"", RSTRING_PTR(mod_name));
+        }
+        break;
 
       case T_DATA:
-	if (RTYPEDDATA_P(obj))
-	    dump_append(dc, ", \"struct\":\"%s\"", RTYPEDDATA_TYPE(obj)->wrap_struct_name);
-	break;
+        if (RTYPEDDATA_P(obj))
+            dump_appendf(dc, ", \"struct\":\"%s\"", RTYPEDDATA_TYPE(obj)->wrap_struct_name);
+        break;
 
       case T_FLOAT:
-	dump_append(dc, ", \"value\":\"%g\"", RFLOAT_VALUE(obj));
-	break;
+        dump_appendf(dc, ", \"value\":\"%g\"", RFLOAT_VALUE(obj));
+        break;
 
       case T_OBJECT:
-	dump_append(dc, ", \"ivars\":%u", ROBJECT_NUMIV(obj));
-	break;
+        dump_appendf(dc, ", \"ivars\":%u", ROBJECT_NUMIV(obj));
+        break;
 
       case T_FILE:
-	fptr = RFILE(obj)->fptr;
-	if (fptr)
-	    dump_append(dc, ", \"fd\":%d", fptr->fd);
-	break;
+        fptr = RFILE(obj)->fptr;
+        if (fptr)
+            dump_appendf(dc, ", \"fd\":%d", fptr->fd);
+        break;
 
       case T_ZOMBIE:
-	dump_append(dc, "}\n");
-	return;
+          dump_append(dc, "}\n");
+          dump_flush(dc);
+          return;
 
       default:
         break;
@@ -304,30 +365,31 @@ dump_object(VALUE obj, struct dump_config *dc)
 
     rb_objspace_reachable_objects_from(obj, reachable_object_i, dc);
     if (dc->cur_obj_references > 0)
-	dump_append(dc, "]");
+        dump_append(dc, "]");
 
     if ((ainfo = objspace_lookup_allocation_info(obj))) {
-	dump_append(dc, ", \"file\":\"%s\", \"line\":%lu", ainfo->path, ainfo->line);
-	if (RTEST(ainfo->mid)) {
-	    VALUE m = rb_sym2str(ainfo->mid);
-	    dump_append(dc, ", \"method\":\"%s\"", RSTRING_PTR(m));
-	}
-	dump_append(dc, ", \"generation\":%"PRIuSIZE, ainfo->generation);
+        dump_appendf(dc, ", \"file\":\"%s\", \"line\":%lu", ainfo->path, ainfo->line);
+        if (RTEST(ainfo->mid)) {
+            VALUE m = rb_sym2str(ainfo->mid);
+            dump_appendf(dc, ", \"method\":\"%s\"", RSTRING_PTR(m));
+        }
+        dump_appendf(dc, ", \"generation\":%"PRIuSIZE, ainfo->generation);
     }
 
     if ((memsize = rb_obj_memsize_of(obj)) > 0)
-	dump_append(dc, ", \"memsize\":%"PRIuSIZE, memsize);
+        dump_appendf(dc, ", \"memsize\":%"PRIuSIZE, memsize);
 
     if ((n = rb_obj_gc_flags(obj, flags, sizeof(flags))) > 0) {
-	dump_append(dc, ", \"flags\":{");
-	for (i=0; i<n; i++) {
-	    dump_append(dc, "\"%s\":true", rb_id2name(flags[i]));
-	    if (i != n-1) dump_append(dc, ", ");
-	}
-	dump_append(dc, "}");
+        dump_append(dc, ", \"flags\":{");
+        for (i=0; i<n; i++) {
+            dump_appendf(dc, "\"%s\":true", rb_id2name(flags[i]));
+            if (i != n-1) dump_append(dc, ", ");
+        }
+        dump_append(dc, "}");
     }
 
     dump_append(dc, "}\n");
+    dump_flush(dc);
 }
 
 static int
@@ -348,11 +410,11 @@ root_obj_i(const char *category, VALUE obj, void *data)
     struct dump_config *dc = (struct dump_config *)data;
 
     if (dc->root_category != NULL && category != dc->root_category)
-	dump_append(dc, "]}\n");
+        dump_append(dc, "]}\n");
     if (dc->root_category == NULL || category != dc->root_category)
-        dump_append(dc, "{\"type\":\"ROOT\", \"root\":\"%s\", \"references\":[\"%#"PRIxVALUE"\"", category, obj);
+        dump_appendf(dc, "{\"type\":\"ROOT\", \"root\":\"%s\", \"references\":[\"%#"PRIxVALUE"\"", category, obj);
     else
-        dump_append(dc, ", \"%#"PRIxVALUE"\"", obj);
+        dump_appendf(dc, ", \"%#"PRIxVALUE"\"", obj);
 
     dc->root_category = category;
     dc->roots = 1;
@@ -397,12 +459,19 @@ dump_output(struct dump_config *dc, VALUE opts, VALUE output, const char *filena
     else {
 	rb_raise(rb_eArgError, "wrong output option: %"PRIsVALUE, output);
     }
+
+    dc->buffer_capa = 4096;
+    dc->buffer_len = 0;
+    dc->buffer = ALLOC_N(char, 4096);
+    
     return output;
 }
 
 static VALUE
 dump_result(struct dump_config *dc, VALUE output)
 {
+    ruby_xfree(dc->buffer);
+
     if (output == sym_string) {
 	return rb_str_resurrect(dc->string);
     }
@@ -475,6 +544,7 @@ objspace_dump_all(int argc, VALUE *argv, VALUE os)
     /* dump roots */
     rb_objspace_reachable_objects_from_root(root_obj_i, &dc);
     if (dc.roots) dump_append(&dc, "]}\n");
+    dump_flush(&dc);
 
     /* dump all objects */
     rb_objspace_each_objects(heap_i, &dc);
