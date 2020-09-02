960594fea14694f1714d2d48f0fec3b02bebcf09
Peter Zhu <peter@peterzhu.ca>
2020-09-02 17:25:35 -0400

    Skip test instead

--- test/ruby/test_gc.rb
+++ test/ruby/test_gc.rb
@@ -428,6 +428,7 @@ class TestGc < Test::Unit::TestCase
   end
 
   def test_vm_object
+    skip "Times out"
     assert_normal_exit <<-'end', '[Bug #12583]', timeout: 120
       ObjectSpace.each_object{|o| o.singleton_class rescue 0}
       ObjectSpace.each_object{|o| case o when Module then o.instance_methods end}
