055ca7b70198878476e06c1a5be8e67df13135ec
Max Bernstein <max@bernsteinbear.com>
2025-07-08 10:55:23 -0400

    ZJIT: Add a BitSet type

---
+++ zjit/src/bitset.rs
@@ -0,0 +1,70 @@
+type Entry = u128;
+
+// TODO(max): Make a `SmallBitSet` and `LargeBitSet` and switch between them if `num_bits` fits in
+// `Entry`.
+pub struct BitSet<T: Into<usize> + Copy> {
+    storage: Vec<Entry>,
+    num_bits: usize,
+    phantom: std::marker::PhantomData<T>,
+}
+
+impl<T: Into<usize> + Copy> BitSet<T> {
+    pub fn with_capacity(num_bits: usize) -> Self {
+        // +1 because we are rounding down
+        let num_entries = num_bits / (Entry::BITS as usize) + 1;
+        Self { storage: vec![0; num_entries], num_bits, phantom: Default::default() }
+    }
+
+    /// Returns whether the value was newly inserted: true if the set did not originally contain
+    /// the bit, and false otherwise.
+    pub fn insert(&mut self, idx: T) -> bool {
+        debug_assert!(idx.into() < self.num_bits);
+        let entry_idx = idx.into() / (Entry::BITS as usize);
+        let bit_idx = idx.into() % (Entry::BITS as usize);
+        let newly_inserted = (self.storage[entry_idx] & (1 << bit_idx)) == 0;
+        self.storage[entry_idx] |= 1 << bit_idx;
+        newly_inserted
+    }
+
+    pub fn get(&self, idx: T) -> bool {
+        debug_assert!(idx.into() < self.num_bits);
+        let entry_idx = idx.into() / (Entry::BITS as usize);
+        let bit_idx = idx.into() % (Entry::BITS as usize);
+        (self.storage[entry_idx] & (1 << bit_idx)) != 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BitSet;
+
+    #[test]
+    #[should_panic]
+    fn get_over_capacity_panics() {
+        let set = BitSet::with_capacity(0);
+        assert_eq!(set.get(0usize), false);
+    }
+
+    #[test]
+    fn with_capacity_defaults_to_zero() {
+        let set = BitSet::with_capacity(4);
+        assert_eq!(set.get(0usize), false);
+        assert_eq!(set.get(1usize), false);
+        assert_eq!(set.get(2usize), false);
+        assert_eq!(set.get(3usize), false);
+    }
+
+    #[test]
+    fn insert_sets_bit() {
+        let mut set = BitSet::with_capacity(4);
+        assert_eq!(set.insert(1usize), true);
+        assert_eq!(set.get(1usize), true);
+    }
+
+    #[test]
+    fn insert_with_set_bit_returns_false() {
+        let mut set = BitSet::with_capacity(4);
+        assert_eq!(set.insert(1usize), true);
+        assert_eq!(set.insert(1usize), false);
+    }
+}
