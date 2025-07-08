297b8a7a853db4432cb2a8bc4c28d45fecd9c411
Max Bernstein <max@bernsteinbear.com>
2025-07-08 14:30:41 -0400

    fixup! ZJIT: Add a BitSet type

--- zjit/src/bitset.rs
+++ zjit/src/bitset.rs
@@ -1,36 +1,37 @@
 type Entry = u128;
 
+const ENTRY_NUM_BITS: usize = Entry::BITS as usize;
+
 // TODO(max): Make a `SmallBitSet` and `LargeBitSet` and switch between them if `num_bits` fits in
 // `Entry`.
 pub struct BitSet<T: Into<usize> + Copy> {
-    storage: Vec<Entry>,
+    entries: Vec<Entry>,
     num_bits: usize,
     phantom: std::marker::PhantomData<T>,
 }
 
 impl<T: Into<usize> + Copy> BitSet<T> {
     pub fn with_capacity(num_bits: usize) -> Self {
-        // +1 because we are rounding down
-        let num_entries = num_bits / (Entry::BITS as usize) + 1;
-        Self { storage: vec![0; num_entries], num_bits, phantom: Default::default() }
+        let num_entries = num_bits.div_ceil(ENTRY_NUM_BITS);
+        Self { entries: vec![0; num_entries], num_bits, phantom: Default::default() }
     }
 
     /// Returns whether the value was newly inserted: true if the set did not originally contain
     /// the bit, and false otherwise.
     pub fn insert(&mut self, idx: T) -> bool {
         debug_assert!(idx.into() < self.num_bits);
-        let entry_idx = idx.into() / (Entry::BITS as usize);
-        let bit_idx = idx.into() % (Entry::BITS as usize);
-        let newly_inserted = (self.storage[entry_idx] & (1 << bit_idx)) == 0;
-        self.storage[entry_idx] |= 1 << bit_idx;
+        let entry_idx = idx.into() / ENTRY_NUM_BITS;
+        let bit_idx = idx.into() % ENTRY_NUM_BITS;
+        let newly_inserted = (self.entries[entry_idx] & (1 << bit_idx)) == 0;
+        self.entries[entry_idx] |= 1 << bit_idx;
         newly_inserted
     }
 
     pub fn get(&self, idx: T) -> bool {
         debug_assert!(idx.into() < self.num_bits);
-        let entry_idx = idx.into() / (Entry::BITS as usize);
-        let bit_idx = idx.into() % (Entry::BITS as usize);
-        (self.storage[entry_idx] & (1 << bit_idx)) != 0
+        let entry_idx = idx.into() / ENTRY_NUM_BITS;
+        let bit_idx = idx.into() % ENTRY_NUM_BITS;
+        (self.entries[entry_idx] & (1 << bit_idx)) != 0
     }
 
     /// Modify `self` to only have bits set if they are also set in `other`. Returns true if `self`
