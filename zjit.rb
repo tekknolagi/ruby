365e880149ee7ec852d36956bf72f909f4c2f323
Max Bernstein <ruby@bernsteinbear.com>
2026-07-06 12:36:10 -0400

    .

--- zjit.rb
+++ zjit.rb
@@ -150,6 +150,7 @@ class << RubyVM::ZJIT
       :compile_side_exit_time_ratio,
       :compile_hir_time_ns,
       :compile_hir_build_time_ns,
+      :compile_hir_remove_trivial_block_params_time_ns,
       :compile_hir_strength_reduce_time_ns,
       :compile_hir_inline_methods_time_ns,
       :compile_hir_canonicalize_time_ns,
